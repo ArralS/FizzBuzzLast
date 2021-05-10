330db9c0d5944093cb4384c37a4bb222b66992ec
ArralS <80539257+ArralS@users.noreply.github.com>
2021-05-10 23:04:58 +0200

    Inicjacja

---
+++ Legion1/Models/Recent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Legion1.Models;
+
+
+namespace Legion1.Models
+{
+    public class Recent
+    {
+        public int ID { get; set; }
+        public String Result { get; set; }
+        
+        public int Number { get; set; }
+       
+        public DateTime Data { get; set; }
+ // xDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD34243435657676547
+
+    }
+}
---
+++ Legion1/Pages/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Legion1.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Legion1.Data;
+
+namespace Legion1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly NumberContext _context;
+        [BindProperty]
+        public FizzBuzz Address { get; set; }
+        
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, NumberContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public String GetResult(int Number)
+        {
+
+            String result = "";
+            if (Number % 3 == 0) result += "Fizz";
+            if (Number % 5 == 0) result += "Buzz";
+            if (result == String.Empty) {
+                result = "Liczba: ";
+                result += Number.ToString();
+                result += " nie spełnia kryteriów Fizz / Buzz";
+
+
+            }
+            return result;
+        }
+        public IActionResult OnPost()
+        {
+                      
+            if (ModelState.IsValid)
+            {
+                Name = GetResult(Address.Number);
+                HttpContext.Session.SetString("SessionAddress",
+                JsonConvert.SerializeObject(new Recent() { Number = Address.Number, Result = Name, Data = DateTime.Now }));
+                _context.Add(new Recent() { Number = Address.Number, Result = Name, Data = DateTime.Now });
+                _context.SaveChanges();
+                return Page();
+            }
+            return Page();
+
+          
+
+        }
+
+    }
+}

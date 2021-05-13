d4695729f8b7ff656a3b4003bc7896753a0bdc07
ArralS <80539257+ArralS@users.noreply.github.com>
2021-05-13 18:47:13 +0200

    Działa xD - Podstawowy

--- Legion1/Data/NumberContext.cs
+++ Legion1/Data/NumberContext.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Legion1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Legion1.Data
 {
-     public class NumberContext : DbContext
-        {
+     public class NumberContext : IdentityDbContext<IdentityUser>
+    {
         public NumberContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Recent> Recent { get; set; }
